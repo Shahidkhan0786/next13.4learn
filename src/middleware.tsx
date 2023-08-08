532bc6a30eaecb2f74ac08fc0639449265fe9ec5
Shahidkhan0786 <shahidkhan0085200@gmail.com>
2023-08-08 07:22:00 +0500

    second commit

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,8 +1,17 @@
-import { NextResponse } from "next/server"
+import { NextResponse, NextRequest } from "next/server"
 
-const Middleware = () => {
-  console.log("Middleware run")
-  return NextResponse.next()
+const Middleware = (request:NextRequest) => {
+  console.log("Middleware run",request.nextUrl.pathname)
+  // return NextResponse.next()
+  // if(request.nextUrl.pathname !== "/login"){
+    return NextResponse.redirect(new URL("/login",request.url))
+  // }
+
 }
 
+export const config = {
+  // if you want to give multiple path 
+  // matcher: ["/about/:path*" , "/blog/:path*"]
+  matcher: "/about/:path*"
+}
 export default Middleware
